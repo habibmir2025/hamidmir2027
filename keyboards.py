04b0cbd9dc4d48cabde887c34d9c9a145bbbd873
habibmir2025 <habibmir202020@gmail.com>
2025-04-19 14:18:22 +0330

    Add files via upload

--- keyboards.py
+++ keyboards.py
@@ -1,11 +1,11 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 def main_menu():
-    keyboard = InlineKeyboardMarkup()
-    keyboard.add(InlineKeyboardButton("📦 مشاهده جیمیل‌ها", callback_data="view_gmails"))
-    return keyboard
+    return InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text="📦 مشاهده جیمیل‌ها", callback_data="view_gmails")]
+    ])
 
 def generate_buy_button(email):
-    keyboard = InlineKeyboardMarkup()
-    keyboard.add(InlineKeyboardButton("💳 پرداخت تستی", callback_data=f"buy_{email}"))
-    return keyboard
+    return InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text="💳 پرداخت تستی", callback_data=f"buy_{email}")]
+    ])
