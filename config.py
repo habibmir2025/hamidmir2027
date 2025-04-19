0c2a71104183891f82564fcc06268542e4b97230
habibmir2025 <habibmir202020@gmail.com>
2025-04-19 14:06:58 +0330

    Add files via upload

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-TOKEN = "7595242671:AAGsUZU9H9OPywe8S9l0pqRGxc58oEUhrSM"
-WEBHOOK_URL = "https://hamidmir2027.onrender.com/webhook"  # Webhook اصلاح شده
+TOKEN = "7867046273:AAHMVkcFBkVMIoPz8is1MLnEYVmB5_EpLPw"
+WEBHOOK_URL = "https://hamidmir2027.onrender.com/webhook"
